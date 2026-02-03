344996c052920b9b1a7ac740376c3633b88e021d
IdanBibi <idanbibi12@gmail.com>
2026-02-04 00:33:07 +0200

    added comments

--- experiment/experiment_navigator.py
+++ experiment/experiment_navigator.py
@@ -11,12 +11,12 @@ def apply_experiment(args: argparse.Namespace, device: str):
     params = {'args': args, 'device': device}
 
     if args.experiment_type == 'paint_dataset':
-        main_paint_dataset(**params)
+        main_paint_dataset(**params) # generate dataset from painter - to train classifier bp
     elif args.experiment_type == 'train_classifier':
-        main_train_classifier(**params)
+        main_train_classifier(**params) # train classifier - check for removal of first/last layer
     elif args.experiment_type == 'attack_pcl':
-        main_attack_pcl(**params)
+        main_attack_pcl(**params) # attack painter ( bpda) classifier
     elif args.experiment_type == 'attack_pcld':
-        main_attack_pcld(**params)
+        main_attack_pcld(**params) # attack whole pipeline
     elif args.experiment_type == 'train_decisioner':
-        main_train_decisioner(**params)
+        main_train_decisioner(**params) # output from pcl + gt
