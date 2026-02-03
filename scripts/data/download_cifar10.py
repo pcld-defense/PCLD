3214b02fc97cd48a7c67a56b170bcc6025d8c87f
IdanBibi <idanbibi12@gmail.com>
2026-02-04 00:06:49 +0200

    created scripts to download datasets

---
+++ scripts/data/download_cifar10.py
@@ -0,0 +1,26 @@
+import os
+
+from tqdm import tqdm
+from huggingface_hub import login
+from datasets import load_dataset
+from PIL import Image
+from dotenv import load_dotenv
+
+
+hf_token = os.getenv("HF_TOKEN")
+login(hf_token)
+ds = load_dataset("uoft-cs/cifar10", split="train", trust_remote_code=True)
+output_dir = "/home/idanbib/PCLD/cifar10/train"
+
+class_names = ds.features["label"].names
+
+print("Saving images to class-specific folders...")
+for i, example in enumerate(tqdm(ds)):
+    image = example["img"]
+    label_idx = example["label"]
+    class_name = class_names[label_idx]
+    class_path = os.path.join(output_dir, class_name)
+    os.makedirs(class_path, exist_ok=True)
+    image.resize((224, 224), resample=Image.LANCZOS).save(os.path.join(class_path, f"{i}.png"))
+
+print(f"Done! Images saved in {output_dir}")
---
+++ scripts/data/download_imagenet.py
@@ -0,0 +1,45 @@
+import os
+
+from huggingface_hub import login
+from datasets import load_dataset
+from PIL import Image
+from dotenv import load_dotenv
+
+from util.consts import IMAGENET_SHAPE
+
+
+hf_token = os.getenv("HF_TOKEN")
+login(hf_token)
+ds = load_dataset("imagenet-1k", split="train", streaming=True, trust_remote_code=True)
+
+images_per_class = 100
+total_classes = 1000
+target_total = images_per_class * total_classes
+output_dir = "/home/idanbib/PCLD/imagenet_100_per_class"
+
+counts = {i: 0 for i in range(total_classes)}
+total_saved = 0
+
+print(f"Starting download: Aiming for {images_per_class} images per class...")
+
+for example in ds:
+    label = example['label']
+
+    if counts[label] < images_per_class:
+        class_path = os.path.join(output_dir, str(label))
+        os.makedirs(class_path, exist_ok=True)
+
+        img_filename = f"{label}_{counts[label]}.jpg"
+        example['image'].convert("RGB").resize((IMAGENET_SHAPE, IMAGENET_SHAPE), Image.Resampling.LANCZOS).save(
+            os.path.join(class_path, img_filename))
+
+        counts[label] += 1
+        total_saved += 1
+
+        if total_saved % 500 == 0:
+            print(f"Saved {total_saved}/{target_total} images...")
+
+    if total_saved >= target_total:
+        break
+
+print("Success! Balanced dataset created.")
