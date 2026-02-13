87f03dfcda8855d3e587b957df048edf44bd6809
IdanBibi <idanbibi12@gmail.com>
2026-02-13 15:50:08 +0200

    modified paths

--- util/consts.py
+++ util/consts.py
@@ -5,12 +5,12 @@ from imagenet_stubs.imagenet_2012_labels import IMAGENET_2012_LABELS
 NUM_OF_HYPHENS = 50
 RESOURCES_DIR = r"/home/idanbib/PCLD/code/resources/"
 RESOURCES_DATASETS_DIR = r"/home/idanbib/PCLD/data/"
-RESOURCES_RESULTS_DIR = RESOURCES_DIR + '/results'
-RESOURCES_MODELS_DIR = RESOURCES_DIR + '/models'
+RESOURCES_RESULTS_DIR = r"/home/idanbib/PCLD/results"
+RESOURCES_MODELS_DIR = r"/home/idanbib/PCLD/models"
 IMAGENET_SHAPE = 224
 
-ACTOR_PATH = os.path.join(RESOURCES_MODELS_DIR, 'painter_actor/actor.pkl')
-RENDERER_PATH = os.path.join(RESOURCES_MODELS_DIR, 'painter_renderer/renderer.pkl')
+ACTOR_PATH = r'/home/idanbib/PCLD/code/resources/models/painter_actor/actor.pkl'
+RENDERER_PATH = r'/home/idanbib/PCLD/code/resources/models/painter_renderer/renderer.pkl'
 
 
 class CIFAR10:
