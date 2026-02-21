369bca011acaf5cdd3988d7c1f40a9bb38a06eae
IdanBibi <idanbibi12@gmail.com>
2026-02-21 21:05:36 +0200

    consts per dataset type

--- util/consts.py
+++ util/consts.py
@@ -7,13 +7,17 @@ RESOURCES_DIR = r"/home/idanbib/PCLD/code/resources/"
 RESOURCES_DATASETS_DIR = r"/home/idanbib/PCLD/data/"
 RESOURCES_RESULTS_DIR = r"/home/idanbib/PCLD/results"
 RESOURCES_MODELS_DIR = r"/home/idanbib/PCLD/models"
-IMAGENET_SHAPE = 224
 
 ACTOR_PATH = r'/home/idanbib/PCLD/code/resources/models/painter_actor/actor.pkl'
 RENDERER_PATH = r'/home/idanbib/PCLD/code/resources/models/painter_renderer/renderer.pkl'
 
+class IMAGENETConsts:
+    SHAPE = 224
+    NUM_CLASSES = 1000
+    MEAN = [0.485, 0.456, 0.406]
+    STD = [0.229, 0.224, 0.225]
 
-class CIFAR10:
+class CIFAR10Consts:
     CIFAR10_MAPPING = {
         0: 'airplane',
         1: 'automobile',
@@ -26,7 +30,8 @@ class CIFAR10:
         8: 'ship',
         9: 'truck'
     }
-    CIFAR_SHAPE = 32
+    SHAPE = 32
+    NUM_CLASSES = 10
 
 
 class PainterConsts:
