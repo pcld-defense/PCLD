c1573127a40b6c9405992690a876847a6ced85af
IdanBibi <idanbibi12@gmail.com>
2026-02-22 12:44:24 +0200

    organized stuff

--- util/attacks.py
+++ util/attacks.py
@@ -4,19 +4,15 @@ import pandas as pd
 import numpy as np
 import random
 import torch
-import torch.nn as nn
-import torch.optim as optim
 from cleverhans.torch.attacks.fast_gradient_method import fast_gradient_method
 from cleverhans.torch.attacks.projected_gradient_descent import projected_gradient_descent
-# from art.attacks.evasion import CarliniLInfMethod#, AutoAttack
-# from art.estimators.classification import PyTorchClassifier
 from autoattack import AutoAttack
 
 
-def attack_batch(model, x, attack, epsilon, attack_nb_iter, targeted, y_classes_targeted,
-                 n_classes, device):
+def attack_batch(model, x, attack, epsilon, attack_nb_iter, targeted, y_classes_targeted, device):
     if epsilon == 0:
         return x
+
     if attack == 'fgsm':
         x_adv = fast_gradient_method(model_fn=model,
                                      x=x,
@@ -39,48 +35,31 @@ def attack_batch(model, x, attack, epsilon, attack_nb_iter, targeted, y_classes_
                                            sanity_checks=False,
                                            clip_min=0,
                                            clip_max=1)
-    elif attack in ('aa', 'cw'):
-        # Define the loss function and the optimizer
-        criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.parameters(), lr=0.001)
-        # Wrap the model with ART's PyTorchClassifier
-        classifier = PyTorchClassifier(
-            model=model,
-            loss=criterion,
-            optimizer=optimizer,
-            input_shape=x.shape[1:],
-            nb_classes=7,
-            clip_values=(0.0, 1.0)
-        )
-
+    elif attack == 'aa':
         # Initialize AutoAttack
-        if attack == 'aa':
-            attack = AutoAttack(estimator=classifier,
-                                eps=epsilon,
-                                norm=np.inf,
-                                parallel=False)
+        adv_attack = AutoAttack(model, norm='Linf', eps=epsilon, version='standard')
+        x_adv = adv_attack.run_standard_evaluation_individual(x, y_classes_targeted)
 
-        elif attack == 'cw':
-            attack = CarliniLInfMethod(classifier=classifier,
-                                       targeted=targeted,
-                                       max_iter=attack_nb_iter,
-                                       batch_size=x.shape[0],
-                                       confidence=0.0,
-                                       #  verbose=False
-                                       )
-
-        # Convert PyTorch tensor to NumPy array
-        x_np = x.detach().cpu().numpy()
-        # Perform the attack
-        x_adv = attack.generate(x=x_np)
-        x_adv = torch.tensor(x_adv, device=device)
+        #
+        # elif attack == 'cw':
+        #     attack = CarliniLInfMethod(classifier=classifier,
+        #                                targeted=targeted,
+        #                                max_iter=attack_nb_iter,
+        #                                batch_size=x.shape[0],
+        #                                confidence=0.0,
+        #                                #  verbose=False
+        #                                )
+    #
+    # x_np = x.detach().cpu().numpy()
+    # # Perform the attack
+    # x_adv = attack.generate(x=x_np)
+    # x_adv = torch.tensor(x_adv, device=device)
 
     return x_adv
 
 
 def attacker(experiment, dataset, attack, adaptive_model, naive_model, run_naive_attack, loader, phase,
-             epsilon, targeted, output_every, n_classes, classes, attack_nb_iter, device,
-             output_type='final_decision'):
+             epsilon, targeted, output_every, classes, attack_nb_iter, device, output_type='final_decision'):
     print(f'run attacks on {phase}...')
     epsilon_real = epsilon / 255.
     output_every_expanded = output_every + [999999]  # add the original image paint step = ∞
@@ -92,13 +71,6 @@ def attacker(experiment, dataset, attack, adaptive_model, naive_model, run_naive
         'dataset': [], 'image': [], 't': [], 'phase': [], 'attacked_model': [], 'defense_model': [],
         'attack': [], 'targeted': [], 'targeted_jumps_allowed': [], 'targeted_label': [], 'norm': [], 'epsilon': [],
         'nb_iter': [], 'actual': [], 'actual_class': [], 'pred': [], 'pred_class': [],
-        f'prob_{classes[0]}': [],
-        f'prob_{classes[1]}': [],
-        f'prob_{classes[2]}': [],
-        f'prob_{classes[3]}': [],
-        f'prob_{classes[4]}': [],
-        f'prob_{classes[5]}': [],
-        f'prob_{classes[6]}': [],
         'attack_time_sec_avg': [],
         'defense_time_sec_avg': []
     }
@@ -127,7 +99,7 @@ def attacker(experiment, dataset, attack, adaptive_model, naive_model, run_naive
             x_adv_naive = attack_batch(naive_model, x, attack, epsilon_real,
                                        attack_nb_iter, targeted,
                                        y_classes_targeted_naive if targeted else y_classes_naive,
-                                       n_classes, device)
+                                       device)
         end_time = time.time()
         attack_naive_time = end_time - start_time
         print(f'attack adaptive BPDA model with surrogate model')
@@ -135,7 +107,7 @@ def attacker(experiment, dataset, attack, adaptive_model, naive_model, run_naive
         x_adv_adaptive = attack_batch(adaptive_model, x, attack, epsilon_real,
                                       attack_nb_iter, targeted,
                                       y_classes_targeted_adaptive if targeted else y_classes_adaptive,
-                                      n_classes, device)
+                                      device)
 
         end_time = time.time()
         attack_adaptive_time = end_time - start_time
@@ -163,7 +135,7 @@ def attacker(experiment, dataset, attack, adaptive_model, naive_model, run_naive
         results_dict['attacked_model'].extend((['naive'] * n) + (['adaptive'] * n))
         results_dict['defense_model'].extend(['adaptive'] * n * 2)
         results_dict['attack'].extend([attack] * n * 2)
-        results_dict['targeted'].extend([int(targeted)] * n * 2)
+        results_dict['targeted'].extend([targeted] * n * 2)
         results_dict['targeted_jumps_allowed'].extend([int(targeted_jumps_allowed)] * n * 2)
         results_dict['targeted_label'].extend(np.repeat(y_classes_targeted, paint_steps).tolist() * 2)
         results_dict['norm'].extend(['linf'] * n * 2)
@@ -173,9 +145,7 @@ def attacker(experiment, dataset, attack, adaptive_model, naive_model, run_naive
         results_dict['actual_class'].extend(np.repeat(y_classes_names, paint_steps).tolist() * 2)
         results_dict['pred'].extend(adv_naive_decisions + adv_adaptive_decisions)
         results_dict['pred_class'].extend(y_pred_classes_names_naive + y_pred_classes_names_adaptive)
-        for pi in range(len(classes)):
-            results_dict[f'prob_{classes[pi]}'].extend(np.array(adv_naive_probs)[:, pi].tolist() + \
-                                                       np.array(adv_adaptive_probs)[:, pi].tolist())
+
         results_dict['attack_time_sec_avg'].extend(
             ([attack_naive_time / batch_size] * n) + ([attack_adaptive_time / batch_size] * n))
         results_dict['defense_time_sec_avg'].extend(
